8ab2312010bc9878667cb2d4050205d013734444
Tanmoy Sen Gupta <36238254+TanmoySG@users.noreply.github.com>
2019-02-16 09:56:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Medical Legacy Directory</title>
+        <link rel="icon" href="favicon.ico" sizes="20x20" type="image/png">  
+        <link rel="stylesheet" type="text/css" href="styling/dashboard.css">
+        <link rel="stylesheet" type="text/css" href="styling/flexboxgrid.css">
+        <link rel="stylesheet" type="text/css" href="styling/typography.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.typekit.net/sgr8dvc.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Open+Sans:300|Oswald|Raleway|Roboto" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    </head>
+    <body>
+        <div class="dashboard-navbar-light">
+            <div class="dashboard-navbar-options-light nav-title" style="width: 100%;">
+                <center>
+                    <span>
+                        <span style="float: left; padding-left: 50px;">Medical Legacy Directory</span>
+                    </span>
+                    <span style="float: right;">
+                        <a href="login.php"><i class="fas fa-sign-in-alt"></i></a>  
+                    </span> 
+                </center>
+            </div>
+        </div>
+
+        <div style="height: 92vh;background-color: #ffee8e">
+            <div style="padding-top: 25vh;">
+                <div class="content-section row" >
+                    <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+                        <span style="font-family: bebas-neue; font-size: 100px; line-height:80px; display: block; vertical-align: middle">Medical Record Keeping<br>made easy!</span>
+                        <p style="font-family: Montserrat; font-size: 30px;">
+                            Our mission is to make Medical Records Keeping easy and efficient that will make treatment more efficient and fast. 
+                        </p>
+                    </div>
+                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                        <img src="illustrations/doctor-colour.svg" alt="doctor" style="width: 80%; height: 80%;  display: block;   margin-left: auto;  margin-right: auto;">
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </body>
+</html>
