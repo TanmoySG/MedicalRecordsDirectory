36535ad8c95eb1f76884e2d61c161bdfbf84d8da
Tanmoy Sen Gupta <36238254+TanmoySG@users.noreply.github.com>
2019-02-21 12:38:13 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -18,28 +18,33 @@
             <div class="dashboard-navbar-options-light nav-title" style="width: 100%;">
                 <center>
                     <span>
-                        <span style="float: left; padding-left: 50px;">Medical Legacy Directory</span>
+                        <a href="index.php" style="float: left; padding-left: 50px; color: black">Medical Legacy Directory</a>
                     </span>
                     <span style="float: right;">
-                        <a href="login.php"><i class="fas fa-sign-in-alt"></i></a>  
+                        <a href="login.php" style="color:#252525"><i class="fas fa-sign-in-alt"></i></a>  
                     </span> 
                 </center>
             </div>
         </div>
 
-        <div style="height: 92vh;background-color: #ffee8e">
-            <div style="padding-top: 25vh;">
-                <div class="content-section row" >
-                    <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
-                        <span style="font-family: bebas-neue; font-size: 100px; line-height:80px; display: block; vertical-align: middle">Medical Record Keeping<br>made easy!</span>
-                        <p style="font-family: Montserrat; font-size: 30px;">
-                            Our mission is to make Medical Records Keeping easy and efficient that will make treatment more efficient and fast. 
-                        </p>
+        <div style="height: 92vh;background-color: #1e78ff; color: white">
+            <div style="transform: translateY(50%)">
+                <div class="content-section" >
+                    <div class="row">
+                        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+                            <span style="font-family: bebas-neue; font-size: 100px; line-height:80px; display: block; vertical-align: middle" class="hero-header-text">Medical Record Keeping<br>made easy!</span>
+                            <p style="font-family: Montserrat; font-size: 30px;">
+                                Our mission is to make Medical Records Keeping easy and efficient that will make treatment more efficient and fast. 
+                            </p>
+                            <div style="float: left;">
+                                <div style="border: solid 1px white; border-radius: 10px; padding: 10px; font-family: Montserrat"><a href="index.php" style="color: white;">Know More</a></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                            <img src="illustrations/doctor-colour.svg" alt="doctor" style="width: 25vw; height: 25vw;  display: block;   margin-left: auto;  margin-right: auto;">
+                        </div> 
                     </div>
-                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-                        <img src="illustrations/doctor-colour.svg" alt="doctor" style="width: 80%; height: 80%;  display: block;   margin-left: auto;  margin-right: auto;">
-                    </div>
-                </div>  
+                </div> 
             </div>
         </div>
     </body>
---
+++ login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login | MLD</title>
+        <link rel="icon" href="favicon.ico" sizes="20x20" type="image/png">  
+        <link rel="stylesheet" type="text/css" href="styling/dashboard.css">
+        <link rel="stylesheet" type="text/css" href="styling/flexboxgrid.css">
+        <link rel="stylesheet" type="text/css" href="styling/typography.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.typekit.net/sgr8dvc.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Open+Sans:300|Oswald|Raleway|Roboto" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    </head>
+    <body style=" background-color: #f1f1f1">
+        <div class="dashboard-navbar-light">
+            <div class="dashboard-navbar-options-light nav-title" style="width: 100%;">
+                <center>
+                    <span>
+                        <a href="index.php" style="float: left; padding-left: 50px; color: black">Medical Legacy Directory</a>
+                    </span>
+                    <span style="float: right;">
+                        <a href="login.php" style="color:#252525"><i class="fas fa-sign-in-alt"></i></a>  
+                    </span> 
+                </center>
+            </div>
+        </div>
+        <div style="height: 92vh;color: #1e78ff; background-color: #f1f1f1">
+            <div style="transform: translateY(20%)">
+                <center><span class="col-12" style="font-family: Montserrat; font-size: 75px;">LOGIN</span></center><br>
+                <div class="content-section" style="font-family: bebas-neue">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <a style="color:  #1e78ff;" href="doctorLogin.php">
+                                <center>
+                                    <i class="fas fa-user-md" style="font-size: 200px;"></i>
+                                    <br><br>
+                                    <span style="font-size: 50px">Doctor</span>
+                                </center>
+                            </a>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <a style="color:  #1e78ff;" href="hospitalLogin.php">
+                                <center>
+                                    <i class="fas fa-hospital" style="font-size: 200px;"></i>
+                                    <br><br>
+                                    <span style="font-size: 50px">Hospital/Clinic</span>
+                                </center>
+                            </a>
+                        </div>  
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <a style="color:  #1e78ff;" href="userLogin.php">
+                                <center>
+                                    <i class="fas fa-user" style="font-size: 200px;"></i>
+                                    <br><br>
+                                    <span style="font-size: 50px">User</span>
+                                </center>
+                            </a>
+                        </div>   
+                    </div>
+                </div> 
+                <br><br><br>
+                <center style="font-family: Montserrat; font-size: 35px;color: black" class="col-12" >New user? <a href="register.php" style="color: #1e78ff ">Register</a></center><br>
+            </div>
+        </div>
+    </body>
+</html>
